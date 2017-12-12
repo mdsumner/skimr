37530ce27411594a980d7653fbad16dc5ec4539c
Michael Sumner <mdsumner@gmail.com>
2017-12-12 23:27:15 +1100

    sf ncoords nparts

---
+++ R/stats_sfc.R
@@ -0,0 +1,107 @@
+#' @include stats_sfc.R
+NULL
+
+
+# Summary statistic functions for sfc
+
+#' ncoords_sf - the complete count of coordinate instances
+#' npaths_sf - complete count of paths, (a ring that is a hole is a path)
+#' @param x A vector
+#' @return count of coordinates, all instances
+#' @export
+ncoords_sf <- function(x) {
+  gibble::gibble(x) %>% 
+    dplyr::summarize(ncoords = sum(nrow)) %>% 
+    dplyr::pull(ncoords) 
+}
+#' @name ncoords_sf
+#' @export
+npaths_sf <- function(x) {
+  gibble::gibble(x) %>% dplyr::tally() %>% dplyr::pull(n)
+}
+sfc_point_funs<-list(
+  missing = n_missing,
+  complete = n_complete,
+  n = length,
+  n_unique = purrr::compose(length, n_unique),
+  valid = purrr::compose(sum, sf::st_is_valid),
+  ncoords = ncoords_sf, 
+  npaths = npaths_sf
+)
+
+sfc_linestring_funs<-list(
+  missing = n_missing,
+  complete = n_complete,
+  n = length,
+  n_unique = purrr::compose(length, n_unique),
+  valid = purrr::compose(sum, sf::st_is_valid),
+  ncoords = ncoords_sf, 
+  npaths = npaths_sf
+  
+)
+
+sfc_polygon_funs<-list(
+  missing = n_missing,
+  complete = n_complete,
+  n = length,
+  n_unique = purrr::compose(length, n_unique),
+  valid = purrr::compose(sum, sf::st_is_valid),
+  ncoords = ncoords_sf, 
+  npaths = npaths_sf
+  
+)
+
+sfc_multipoint_funs<-list(
+  missing = n_missing,
+  complete = n_complete,
+  n = length,
+  n_unique = purrr::compose(length, n_unique),
+  valid = purrr::compose(sum, sf::st_is_valid),
+  ncoords = ncoords_sf, 
+  npaths = npaths_sf
+  
+)
+
+sfc_multilinestring_funs<-list(
+  missing = n_missing,
+  complete = n_complete,
+  n = length,
+  n_unique = purrr::compose(length, n_unique),
+  valid = purrr::compose(sum, sf::st_is_valid),
+  ncoords = ncoords_sf, 
+  npaths = npaths_sf
+  
+)
+
+sfc_multipolygon_funs<-list(
+  missing = n_missing,
+  complete = n_complete,
+  n = length,
+  n_unique = purrr::compose(length, n_unique),
+  valid = purrr::compose(sum, sf::st_is_valid),
+  ncoords = ncoords_sf, 
+  npaths = npaths_sf
+  
+)
+
+sfc_geometry_funs<-list(
+  missing = n_missing,
+  complete = n_complete,
+  n = length,
+  n_unique = purrr::compose(length, n_unique),
+  valid = purrr::compose(sum, sf::st_is_valid),
+  ncoords = ncoords_sf, 
+  npaths = npaths_sf
+  
+)
+
+
+skim_with(
+  sfc_POINT = sfc_point_funs,
+  sfc_LINESTRING = sfc_linestring_funs,
+  sfc_POLYGON = sfc_polygon_funs,
+  sfc_MULTIPOINT = sfc_multipoint_funs,
+  sfc_MULTILINESTRING = sfc_multilinestring_funs,
+  sfc_MULTIPOLYGON = sfc_multipolygon_funs,
+  sfc_GEOMETRY = sfc_geometry_funs
+)
